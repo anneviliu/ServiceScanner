07c89329911ec38d979e36a867b98fdb15d559e2
Annevi <anneviliu@gmail.com>
2020-07-30 13:16:26 +0800

    小修改

--- main.go
+++ main.go
@@ -2,17 +2,52 @@ package main
 
 import (
 	"F-Scrack-Go/serverScan/icmpcheck"
+	"F-Scrack-Go/serverScan/portscan"
 	"fmt"
+	"github.com/fatih/color"
+	"log"
 )
 
 var hostList []string
-var DEBUG bool = true
+var aliveList []string
+var aliveAddr []string
+var isIP bool
 
 func main() {
-	if hostinfile != "" && !DEBUG {
-		hostList = StandardHostsViaFile(hostinfile)
-	} else if DEBUG {
-		hostList = StandardHostsViaFile("test.txt")
+	// 避免同时使用 -hf 和 -h
+	if hostinfile != "" && hosts != "" {
+		log.Fatal("Can not use -hf and -h at the same time.")
 	}
-	fmt.Println(icmpcheck.ICMPRun(hostList))
+
+	// -hf
+	if hostinfile != "" {
+		//hostList = StandardIPViaFile(hostinfile, "file")
+		hostList = StandardIPViaFile("test.txt", "file")
+		aliveList = icmpcheck.ICMPRun(hostList)
+		//fmt.Println(aliveList)
+	}
+
+	// -h
+	if hosts != "" {
+		// 标准化ip
+		hostList = StandardIPViaFile(hosts, "single")
+		//hostList = StandardIPViaFile("192.168.1.1", "single")
+		if model == "" {
+			// icmp 存活探测
+			aliveList = icmpcheck.ICMPRun(hostList)
+			for _, host := range aliveList {
+				fmt.Printf("(ICMP) Target '%s' is alive\n", host)
+			}
+			portscan.TCPportScan(aliveList, ports, "tcp", timeout)
+			//icmp
+			//_, aliveAddress := portscan.TCPportScan(aliveList, ports, model, timeout)
+			//fmt.Println(aliveAddress)
+
+		} else if model == "tcp" {
+			a, _ := portscan.TCPportScan(aliveList, "80", "tcp", 2)
+			fmt.Println(a)
+		}
+	}
+
+	//fmt.Println(icmpcheck.ICMPRun(hostList))
 }
--- utils.go
+++ utils.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"github.com/malfunkt/iprange"
 	"log"
 	"os"
+	"regexp"
 )
 
 var line []string
@@ -30,23 +32,74 @@ func ReadFile(textfile string) ([]string, error) {
 	return line, err
 }
 
-func CheckIp(ip string) {
+func CheckHosts(hosts string) bool {
+	hostsPattern := `^(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])\/(\d{1}|[0-2]{1}\d{1}|3[0-2])$|^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$`
+	hostsRegexp := regexp.MustCompile(hostsPattern)
+	checkHost := hostsRegexp.MatchString(hosts)
 
+	hostsPattern2 := `\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})\-((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2}))\b`
+	hostsRegexp2 := regexp.MustCompile(hostsPattern2)
+	checkHost2 := hostsRegexp2.MatchString(hosts)
+
+	hostsPattern3 := `((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(\*$)`
+	hostsRegexp3 := regexp.MustCompile(hostsPattern3)
+	checkHost3 := hostsRegexp3.MatchString(hosts)
+
+	if hosts == "" || (checkHost == false && checkHost2 == false && checkHost3 == false) {
+		flag.Usage()
+		return false
+	}
+	return true
 }
 
-func StandardHostsViaFile(filePath string) []string {
-	var standardHostList []string
-	hostList, err := ReadFile(filePath)
-	if err != nil {
-		log.Fatal(err)
+func CheckPorts(ports string) bool {
+	portsPattern := `^([0-9]|[1-9]\d|[1-9]\d{2}|[1-9]\d{3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$|^\d+(-\d+)?(,\d+(-\d+)?)*$`
+	portsRegexp := regexp.MustCompile(portsPattern)
+	checkPort := portsRegexp.MatchString(ports)
+	if ports != "" && checkPort == false {
+		flag.Usage()
+		return false
 	}
-	for _, v := range hostList {
-		ipRng, err := iprange.ParseList(v)
+	return true
+}
+
+/*
+ * @param  host string 传入需要处理的ip  t string 传入IP的方式 （文件\命令行输入）
+ * @return []string 返回IP列表
+ * @auth Annevi
+ */
+
+func StandardIPViaFile(host string, t string) []string {
+	var standardHostList []string
+	if t == "file" {
+		hostList, err := ReadFile(host)
 		if err != nil {
 			log.Fatal(err)
 		}
-		for _, v := range ipRng.Expand() {
-			standardHostList = append(standardHostList, v.String())
+		for _, v := range hostList {
+			if CheckHosts(v) {
+				ipRng, err := iprange.ParseList(v)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, v := range ipRng.Expand() {
+					standardHostList = append(standardHostList, v.String())
+				}
+			} else {
+				log.Fatal("Hosts format error")
+			}
+		}
+	} else if t == "single" {
+		if CheckHosts(host) {
+			ipRng, err := iprange.ParseList(host)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, v := range ipRng.Expand() {
+				standardHostList = append(standardHostList, v.String())
+			}
+		} else {
+			log.Fatal("Hosts format error")
 		}
 	}
 	return standardHostList
